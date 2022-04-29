1f53fc499c39ecbf1455bd0ad56d1d8d14a56b0d
Shelby Constant <shelbsconstant@gmail.com>
2022-04-29 11:28:42 -0400

    commit

--- p2/src/components/favImage.js
+++ p2/src/components/favImage.js
@@ -5,7 +5,7 @@ import '../pretty.css'
 export default class IndexThree extends React.Component{
     render(){
         return(
-            <img className="img" src= {require('.//images/arc1.jpg')} alt = "pic"/>
+            <img className="img" src= {require('../images/arc1.jpg')} alt = "pic"/>
         )
            
     }
