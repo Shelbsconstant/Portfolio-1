074b0b2bd49c7f313af5c6bf90784d87d2e09335
Shelby Constant <shelbsconstant@gmail.com>
2022-04-29 04:14:03 -0400

    commit

---
+++ p2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <Example show="Arcane" charcter="Jinx" />
+    </div>
+  );
+}
+
+export default App;
